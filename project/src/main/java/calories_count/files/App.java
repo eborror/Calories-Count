52ec975b6fb1c472cfdf982bd163788ed0a00ba6
Gemma1111 <150280048+Gemma1111@users.noreply.github.com>
2025-06-19 13:30:51 -0400

    Update App.java to make tracked food the main screen

--- project/src/main/java/calories_count/files/App.java
+++ project/src/main/java/calories_count/files/App.java
@@ -1,3 +1,9 @@
+//***********************************************************************
+//   App.java           House Tully
+//
+//  The App class invokes the main method to start the application.
+//***********************************************************************
+
 package calories_count.files;
 
 import javafx.application.Application;
@@ -12,18 +18,21 @@ public class App extends Application {
     private static Scene scene;
     public static User currentUser;
 
+    // Start the App and set opening scene
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 415, 620);
+        scene = new Scene(loadFXML("TrackedFoods"), 415, 620);
         stage.setScene(scene);
         stage.setTitle("Calories Count");
         stage.show();
     }
 
+    // Switches the current scene to a different fxml view
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
 
+    // Loads fxml file
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();

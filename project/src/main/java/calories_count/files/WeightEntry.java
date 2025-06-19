d5e06e65f12ab5e05bbf97f307810f4fd09af6f1
Gemma1111 <150280048+Gemma1111@users.noreply.github.com>
2025-06-19 14:01:16 -0400

    Update WeightEntry.java documentation

--- project/src/main/java/calories_count/files/WeightEntry.java
+++ project/src/main/java/calories_count/files/WeightEntry.java
@@ -1,3 +1,9 @@
+//***********************************************************************
+//   WeightEntry.java           House Tully
+//
+//  The WeightEntry class represents an entry in the user's weight log.
+//***********************************************************************
+
 package calories_count.files;
 
 import java.io.Serializable;
@@ -7,16 +13,14 @@ public class WeightEntry implements Serializable {
     private LocalDate date;
     private double weight;
 
+    // Saves the date and weight # (in lbs) in an entry
     public WeightEntry(LocalDate date, double weight) {
         this.date = date;
         this.weight = weight;
     }
 
-    public LocalDate getDate() { return date; }
-    public double getWeight() { return weight; }
-
     @Override
     public String toString() {
-        return date + ": " + weight + " lbs";
+        return date + ": " + weight + " lbs"; 
     }
 }
